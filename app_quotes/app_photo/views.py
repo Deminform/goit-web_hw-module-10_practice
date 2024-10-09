9a3f90f12736921270dcc24c38a7f29fa8fdf460
Deminform <deminform@gmail.com>
2024-10-09 02:29:10 +0200

    0.1.4

--- app_quotes/app_photo/views.py
+++ app_quotes/app_photo/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app_photo/index.html', context={'msg': 'Hello World!'})
+
+
+def pictures(request):
+    return render(request, 'app_photo/pictures.html', context={'pictures': 'Hello World!'})
+
+
+def upload(request):
+    return render(request, 'app_photo/upload.html', context={'upload': 'Hello World!'})
